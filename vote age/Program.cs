b6170c713a5dca8100b302e0717a943b52ace6bf
akashkuchankar <kuchankarakash@gmail.com>
2022-09-30 06:23:40 -0700

    Add project files.

---
+++ vote age/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vote_age
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the age");
+            int age = Convert.ToInt32(Console.ReadLine());
+            if (age >= 18)
+            {
+                Console.WriteLine(" valid for voting")
+                    ;
+            }
+            else
+            {
+                Console.WriteLine("not valid for voting")
+                    ;
+            }
+        }
+    }
+}
